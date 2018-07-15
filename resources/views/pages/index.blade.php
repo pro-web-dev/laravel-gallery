6892b488d0f149b6d56a6f10a9cf3213fbf582ab
opentest00 <opentest00@mail.ru>
2018-07-16 00:06:36 +0500

    test

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('content')
+    <div class="container">
+        <h1 class="text-center mb-3">My Gallery</h1>
+        <div class="row">
+            <div class="col-lg-3 col-sm-6 my-2">
+                <div class="box">
+                    <img src="/img/1.jpg" class="img-thumbnail">
+                    <a href="{{ route('gallery.show', 2) }}" class="btn btn-info d-block my-2">Show <i class="fa fa-eye"></i></a>
+                    <a href="{{ route('gallery.edit', 2) }}" class="btn btn-warning d-block my-2">Edit <i class="fa fa-edit"></i></a>
+                    <a href="" class="btn btn-danger d-block my-2">Delete <i class="fa fa-remove"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
